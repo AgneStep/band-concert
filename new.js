ea0b71c536285e10b5c303b96329a491b2d90836
AgneStep <127443404+AgneStep@users.noreply.github.com>
2023-03-13 18:48:31 +0000

    Add files via upload

---
+++ new.js
@@ -0,0 +1,19 @@
+const barWidth = 20;
+let lastBar = -1;
+
+function setup() {
+  createCanvas(displayWidth, 60);
+  colorMode(HSB, width, height, 100);
+  noStroke();
+}
+
+
+function draw() {
+  let whichBar = mouseX / barWidth;
+  if (whichBar !== lastBar) {
+    let barX = whichBar * barWidth;
+    fill(barX, mouseY, 66);
+    rect(barX, 0, barWidth, height);
+    lastBar = whichBar;
+  }
+}
